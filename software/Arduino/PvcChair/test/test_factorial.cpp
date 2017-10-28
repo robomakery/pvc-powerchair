96d83d676dd04573cb1ad926d147524972a46b87
Dylan Vaughn <dylancvaughn@gmail.com>
2017-10-28 01:38:42 -0700

    adding catch testing framework

--- software/Arduino/PvcChair/test/test_factorial.cpp
+++ software/Arduino/PvcChair/test/test_factorial.cpp
@@ -1,4 +1,3 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
 unsigned int Factorial( unsigned int number ) {
@@ -6,6 +5,7 @@ unsigned int Factorial( unsigned int number ) {
 }
 
 TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 0 );
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
